f68cff4d8f5b48e45821c1bbe7860a42896d67f3
whitelistthingloliamsosecretnoonecanseemeimsocoollolreallyareusurelolhowlongcanimakethis <147185847+whitelistbotthinglol@users.noreply.github.com>
2024-06-07 21:50:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,46 +4,45 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Middleware to check for custom header
 const checkHeader = (req, res, next) => {
-  const customHeader = req.headers['client']; // Change header name here
-  if (customHeader === 'roadblocks') { // Change expected value here
+  const customHeader = req.headers['client'];
+  if (customHeader === 'roadblocks') {
     next();
   } else {
     res.status(403).send('Forbidden');
   }
 };
 
-// Route to return content of response.txt if valid key is provided
-app.get('/api/auth', checkHeader, (req, res) => {
+const checkAuth = (req, res, next) => {
   const keysFilePath = path.join(__dirname, 'keys.json');
-  const responseFilePath = path.join(__dirname, 'response.txt');
+  const jsonData = JSON.parse(fs.readFileSync(keysFilePath, 'utf8'));
+  const keys = jsonData.keys;
+  const providedKey = req.query.key;
+  const version = req.query.version;
 
-  fs.readFile(keysFilePath, 'utf8', (err, data) => {
+  if (providedKey && keys.includes(providedKey)) {
+    req.authType = 'key';
+    next();
+  } else if (version === 'free') {
+    req.authType = 'free';
+    next();
+  } else {
+    res.status(403).send('Forbidden');
+  }
+};
+
+app.get('/api/auth', checkAuth, checkHeader, (req, res) => {
+  const responseFilePath = req.authType === 'free' ? 'special-response.txt' : 'response.txt';
+
+  fs.readFile(path.join(__dirname, responseFilePath), 'utf8', (err, responseData) => {
     if (err) {
-      res.status(500).send('Error reading keys file');
+      res.status(500).send('Error reading response file');
       return;
     }
-    
-    const jsonData = JSON.parse(data);
-    const keys = jsonData.keys;
-    const providedKey = req.query.key;
-
-    if (providedKey && keys.includes(providedKey)) {
-      fs.readFile(responseFilePath, 'utf8', (err, responseData) => {
-        if (err) {
-          res.status(500).send('Error reading response file');
-          return;
-        }
-        res.send(responseData);
-      });
-    } else {
-      res.status(403).send('Invalid key');
-    }
+    res.send(responseData);
   });
 });
 
-// Route to return keys
 app.get('/api/keys-1ynmluP5V5xqBZMBRsE6vhsrTobzyNGi', checkHeader, (req, res) => {
   const keysFilePath = path.join(__dirname, 'keys.json');
 
